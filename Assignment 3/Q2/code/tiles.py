af484a8f782a537bea866503c998b4e759d93ad4
michael-lewis1 <S342724@students.cdu.edu.au>
2024-02-11 10:07:17 +0930

    Added Brick

--- Assignment 3/Q2/code/tiles.py
+++ Assignment 3/Q2/code/tiles.py
@@ -3,8 +3,8 @@ import pygame
 class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,size):
         super().__init__()
-        self.image = pygame.Surface((size,size))
-        self.image.fill('grey')
+        self.image = pygame.image.load("Assignment 3\Q2\graphics\Tile_36.png")
+        #self.image = pygame.Surface((size,size))
         self.rect = self.image.get_rect(topleft = pos)
 
     def update(self,x_shift):
