36cd540f019418eb387a7cf65b37e36c8ce0265d
michael-lewis1 <S342724@students.cdu.edu.au>
2024-02-11 10:07:38 +0930

    Old Placeholder

--- Assignment 3/Q2/code/tiles.py
+++ Assignment 3/Q2/code/tiles.py
@@ -4,6 +4,7 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,size):
         super().__init__()
         self.image = pygame.image.load("Assignment 3\Q2\graphics\Tile_36.png")
+        #self.image.fill('grey')
         #self.image = pygame.Surface((size,size))
         self.rect = self.image.get_rect(topleft = pos)
 
