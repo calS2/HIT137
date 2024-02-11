0ba6159e0f5e66f70b51831a262466cbbee46e7c
Harry Whaley <s364814@students.cdu.edu.au>
2024-02-11 14:05:11 +0930

    add enemy class & images

--- Assignment 3/Q2/code/tiles.py
+++ Assignment 3/Q2/code/tiles.py
@@ -1,4 +1,5 @@
 import pygame
+from support import import_folder
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,size,kind):
@@ -12,8 +13,25 @@ class Tile(pygame.sprite.Sprite):
             self.rect = self.image.get_rect(center = (pos[0]+32,pos[1]+32))
             self.collectable = True
         #self.image.fill('grey')
-        #self.image = pygame.Surface((size,size))
-        
+        #self.image = pygame.Surface((size,size))    
 
     def update(self,x_shift):
         self.rect.x += x_shift
+
+
+class AnimatedTile(Tile):
+	def __init__(self,size,x,y,path):
+		super().__init__(size,x,y)
+		self.frames = import_folder(path)
+		self.frame_index = 0
+		self.image = self.frames[self.frame_index]
+
+	def animate(self):
+		self.frame_index += 0.15
+		if self.frame_index >= len(self.frames):
+			self.frame_index = 0
+		self.image = self.frames[int(self.frame_index)]
+
+	def update(self,shift):
+		self.animate()
+		self.rect.x += shift
